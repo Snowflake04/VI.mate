a47d047f3704ba93cae4bbab8f7bdc18a8591e54
Snowflake <76170076+Snowflake04@users.noreply.github.com>
2024-01-28 23:24:20 +0530

    Update SocketProvider.jsx

--- client/src/context/SocketProvider.jsx
+++ client/src/context/SocketProvider.jsx
@@ -9,7 +9,7 @@ export const useSocket = () => {
 };
 
 export const SocketProvider = ({children}) => {
-  const socket = useMemo(() => io('http://localhost:8000'), []);
+  const socket = useMemo(() => io('https://server-ipb4.onrender.com'), []);
 
   return (
     <SocketContext.Provider value={socket}>
